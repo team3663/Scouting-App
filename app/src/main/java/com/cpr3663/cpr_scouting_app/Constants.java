922d0375ec2436124b34db03fdd6a492c1925278
Adriel Stammler <jstammler@msn.com>
2024-08-24 01:42:18 -0700

    Re-factored project files
    Moved classes out to their own file.  Created a Globals and Constants class and fixed up code references.

---
+++ app/src/main/java/com/cpr3663/cpr_scouting_app/Constants.java
@@ -0,0 +1,11 @@
+package com.cpr3663.cpr_scouting_app;
+
+public class Constants {
+    public static final String NO_TEAM = "No Team Exists"; // use to check if no team exists for a given team number
+    public static final String NO_MATCH = "No Match"; // use to check if no match info exists for a given match number
+    public static final String NO_COMPETITION = "No Competition Name Exists"; // use to check if no competition info exists for a given competition id
+    public static final int NO_EVENT = 999; // use to check if the eventID you're looking for doesn't exist
+    public static final String PHASE_AUTO = "Auto";
+    public static final String PHASE_TELEOP = "Teleop";
+    public static final String PHASE_NONE = "";
+}
---
+++ app/src/main/java/com/cpr3663/cpr_scouting_app/Globals.java
@@ -0,0 +1,13 @@
+package com.cpr3663.cpr_scouting_app;
+
+import java.util.ArrayList;
+
+public class Globals {
+    public static ArrayList<String> TeamList = new ArrayList<String>();
+    public static Competitions CompetitionList = new Competitions();
+    public static Matches MatchList = new Matches();
+    public static Devices DeviceList = new Devices();
+    public static DNPs DNPList = new DNPs();
+    public static Events EventList = new Events();
+
+}
