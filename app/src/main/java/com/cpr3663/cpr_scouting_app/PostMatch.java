8f01a5ec5452e2153e35498325124a84fb232401
Adriel Stammler <adriel.stammler.stu@cedarpark.org>
2024-09-10 21:19:41 -0700

    Combine Comments and DNP
    Since we don't have any DNP Reasons at the moment I just got rid of it entirely.

--- app/src/main/java/com/cpr3663/cpr_scouting_app/PostMatch.java
+++ app/src/main/java/com/cpr3663/cpr_scouting_app/PostMatch.java
@@ -37,15 +37,10 @@ public class PostMatch extends AppCompatActivity {
     // =============================================================================================
     private PostMatchBinding postMatchBinding;
     TextView drop_Comments;
-    TextView drop_DNP;
     boolean[] selectedComment;
-    boolean[] selectedDNPReasons;
     //Creating an array list for the Comments
     ArrayList<Integer> CommentList = new ArrayList<>();
     String[] CommentArray = Globals.CommentList.getDescriptionList();
-    //Creating an array list for the DNP reasons
-    ArrayList<Integer> DNPReasonsList = new ArrayList<>();
-    String[] DNPReasonsArray = Globals.DNPList.getDescriptionList();
 
     // Doesn't appear to be needed on Tablet but helps on Virtual Devices.
     @SuppressLint({"DiscouragedApi", "SetTextI18n", "ClickableViewAccessibility", "ResourceAsColor"})
@@ -120,11 +115,9 @@ public class PostMatch extends AppCompatActivity {
 
         // assign variable
         drop_Comments = postMatchBinding.dropComments;
-        drop_DNP = postMatchBinding.dropDNP;
 
-        // initialize comment and dnp reasons arrays
+        // initialize comment reasons arrays
         selectedComment = new boolean[CommentArray.length];
-        selectedDNPReasons = new boolean[DNPReasonsArray.length];
 
         drop_Comments.setText("0 " + getResources().getString(R.string.dropdown_items_selected));
         //code for how to open the dropdown menu when clicked and select items
@@ -213,102 +206,11 @@ public class PostMatch extends AppCompatActivity {
             }
         });
 
-        drop_DNP.setText("0 " + getResources().getString(R.string.dropdown_items_selected));
-        // code for how to open the DNP dropdown and select items
-        drop_DNP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(PostMatch.this);
-
-                // set title for the dropdown
-                builder.setTitle("Select Reason(s)");
-
-
-                // set dialog non cancelable
-                builder.setCancelable(false);
-
-                // adds the predetermined DNP reasons to the dropdown
-                builder.setMultiChoiceItems(DNPReasonsArray, selectedDNPReasons, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
-                        // check condition
-                        if (b) {
-                            // when checkbox selected
-                            // Add position  in DNP Reasons list
-                            DNPReasonsList.add(i);
-                            // Sort array list
-                            Collections.sort(DNPReasonsList);
-                        } else {
-                            // when checkbox unselected
-                            // Remove position from DNP Reasons list
-                            DNPReasonsList.remove(Integer.valueOf(i));
-                        }
-                    }
-                });
-
-                // adds the "ok" button to the dropdown menu, allowing you to exit the menu
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // Initialize string builder
-                        StringBuilder stringBuilder = new StringBuilder();
-                        // use for loop
-                        for (int j = 0; j < DNPReasonsList.size(); j++) {
-                            // concat array value
-                            stringBuilder.append(DNPReasonsArray[DNPReasonsList.get(j)]);
-                            // check condition
-                            if (j != DNPReasonsList.size() - 1) {
-                                // When j value  not equal
-                                // to DNP Reasons list size - 1
-                                // add comma
-                                stringBuilder.append(", ");
-                            }
-                        }
-                        // set number of selected on DNPTextView
-                        drop_DNP.setText(DNPReasonsList.size() + " " + getResources().getString(R.string.dropdown_items_selected));
-                    }
-                });
-
-                // adds the "cancel" button to the dropdown, allowing you to exit the
-                // menu without having any selected items
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // dismiss dialog
-                        dialogInterface.dismiss();
-                    }
-                });
-
-                // adds the "clear all" button, allowing you to exit the menu after
-                // clearing all previously selected items
-                builder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // use for loop
-                        for (int j = 0; j < selectedDNPReasons.length; j++) {
-                            // remove all selection
-                            selectedDNPReasons[j] = false;
-                            // clear DNP Reasons list
-                            DNPReasonsList.clear();
-                            // clear text view value and reset to the title
-                            drop_DNP.setText("0 " + getResources().getString(R.string.dropdown_items_selected));
-                        }
-                    }
-                });
-                // show dialog
-                builder.show();
-
-            }
-        });
-
         // Create Components
         // TODO: Change type for drop downs once we have the right XML and Java for it.
         CheckBox check_DidLeave = postMatchBinding.checkboxDidLeave;
         Spinner drop_ClimbPosition = postMatchBinding.spinnerClimbPosition;
         Spinner drop_Trap = postMatchBinding.spinnerTrap;
-        TextView drop_DNP = postMatchBinding.dropDNP;
         TextView drop_Comments = postMatchBinding.dropComments;
 
         // Since we are putting the checkbox on the RIGHT side of the text, the checkbox doesn't honor padding.
@@ -329,7 +231,6 @@ public class PostMatch extends AppCompatActivity {
 //                Globals.EventLogger.LogData(Constants.LOGKEY_CLIMB_POSITION, postMatchBinding.spinnerClimbPosition.getSelectedItem().toString());
 //                Globals.EventLogger.LogData(Constants.LOGKEY_TRAP, postMatchBinding.spinnerTrap.getSelectedItem().toString());
                 // TODO : need to know how to build a multi-selected list of IDs (delimiter will be ":")
-//                Globals.EventLogger.LogData(Constants.LOGKEY_DNPS, postMatchBinding.dropDNP.toString());
 //                Globals.EventLogger.LogData(Constants.LOGKEY_COMMENTS, postMatchBinding.dropComments.toString());
 
                 // We're done with the logger
