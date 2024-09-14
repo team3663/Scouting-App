2e5fd4e5e3790d2c8a494b4e9a057415b00b6ccb
Adriel Stammler <adriel.stammler.stu@cedarpark.org>
2024-09-13 20:18:22 -0700

    trim scouter name
    To increase probability that it matches with what you type in other times.

--- app/src/main/java/com/cpr3663/cpr_scouting_app/PreMatch.java
+++ app/src/main/java/com/cpr3663/cpr_scouting_app/PreMatch.java
@@ -181,7 +181,7 @@ public class PreMatch extends AppCompatActivity {
 
                         // Log all of the data from this page
                         Globals.EventLogger.LogData(Constants.LOGKEY_TEAM_TO_SCOUT, preMatchBinding.editTeamToScout.getText().toString());
-                        Globals.EventLogger.LogData(Constants.LOGKEY_SCOUTER, preMatchBinding.editScouterName.getText().toString().toUpperCase());
+                        Globals.EventLogger.LogData(Constants.LOGKEY_SCOUTER, preMatchBinding.editScouterName.getText().toString().toUpperCase().trim());
                         Globals.EventLogger.LogData(Constants.LOGKEY_DID_PLAY, String.valueOf(preMatchBinding.checkboxDidPlay.isChecked()));
                         Globals.EventLogger.LogData(Constants.LOGKEY_TEAM_SCOUTING, String.valueOf(Globals.CurrentScoutingTeam));
                         if (checkbox_DidPlay.isChecked())
