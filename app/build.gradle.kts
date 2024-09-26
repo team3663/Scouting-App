01ea09c0281a2ca05166ae2825fea38c67f4c532
Adriel Stammler <jstammler@msn.com>
2024-09-26 01:05:13 -0700

    new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cpr3663.cpr_scouting_app"
         minSdk = 30
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
         setProperty("archivesBaseName", "CPR-Scout-$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
