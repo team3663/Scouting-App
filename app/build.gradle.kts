8fcfd080014f9d60e7c1701c6a19d7631cdd7bf2
Adriel Stammler <jstammler@msn.com>
2025-02-06 23:17:11 -0800

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.team3663.scouting_app"
         minSdk = 30
         targetSdk = 34
-        versionCode = 21
-        versionName = "2.1.5"
+        versionCode = 22
+        versionName = "2.2.0"
 
         setProperty("archivesBaseName", "CPR-Scout-$versionName")
 
