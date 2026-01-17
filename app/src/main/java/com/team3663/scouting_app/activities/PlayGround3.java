b6f6babe401f6548fd0efd6dc8b0152bdab1ad33
Adriel Stammler <jstammler@msn.com>
2026-01-17 14:04:32 -0800

    3

--- app/src/main/java/com/team3663/scouting_app/activities/PlayGround3.java
+++ app/src/main/java/com/team3663/scouting_app/activities/PlayGround3.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +18,8 @@ import com.team3663.scouting_app.databinding.Playground3Binding;
 public class PlayGround3 extends AppCompatActivity {
 
     private Playground3Binding playgroundBinding;
+    private int tens = 0;
+    private int ones = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,30 +37,7 @@ public class PlayGround3 extends AppCompatActivity {
 
         initQuit();
         initNext();
-
-        SeekBar seekbar = findViewById(R.id.seekBar);
-        seekbar.setHapticFeedbackEnabled(true);
-        TextView textView = findViewById(R.id.textView);
-
-        seekbar.setProgress(8);
-        seekbar.setMax(60);
-        textView.setText(String.valueOf(seekbar.getProgress()));
-
-        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textView.setText(String.valueOf(progress));
-
-            }
-        });
+        initButtons();
     }
 
     // =============================================================================================
@@ -101,5 +78,45 @@ public class PlayGround3 extends AppCompatActivity {
 
             finish();
         });
+    }
+
+    // =============================================================================================
+    // Function:    initButtons
+    // Description: Initialize the Number buttons
+    // Parameters:  void
+    // Output:      void
+    // =============================================================================================
+    private void initButtons() {
+        initButton(playgroundBinding.but90, 90);
+        initButton(playgroundBinding.but80, 80);
+        initButton(playgroundBinding.but70, 70);
+        initButton(playgroundBinding.but9, 9);
+        initButton(playgroundBinding.but8, 8);
+    }
+
+    // =============================================================================================
+    // Function:    initButton
+    // Description: Initialize an individual buttons
+    // Parameters:  void
+    // Output:      void
+    // =============================================================================================
+    private void initButton(Button in_button, int in_id) {
+        in_button.setBackgroundColor(R.color.dark_grey);
+
+        in_button.setId(in_id);
+        in_button.setOnClickListener(view1 -> processButton(in_button.getId()));
+    }
+
+    // =============================================================================================
+    // Function:    processButton
+    // Description: Process a button click
+    // Parameters:  void
+    // Output:      void
+    // =============================================================================================
+    private void processButton(int in_id) {
+        if (in_id > 9) tens = in_id;
+        else ones = in_id;
+
+        playgroundBinding.textView.setText(String.valueOf(tens + ones));
     }
 }
