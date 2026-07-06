b03620da0952604591b9a59fdcb5b4fd7a0d1c8c
Jeff Stammler <jstammler@msn.com>
2026-07-06 14:18:11 -0700

    added defaults for network fields

--- app/src/main/java/com/team3663/scouting_app/fragments/SettingsPage3.java
+++ app/src/main/java/com/team3663/scouting_app/fragments/SettingsPage3.java
@@ -57,11 +57,11 @@ public class SettingsPage3 extends Fragment {
     // Output:      void
     // =============================================================================================
     private void initFields() {
-        binding.editGoogleDrive.setText(Globals.sp.getString(Constants.Prefs.GOOGLE_DRIVE, ""));
-        binding.editServer.setText(Globals.sp.getString(Constants.Prefs.SQL_SERVER, ""));
-        binding.editDatabase.setText(Globals.sp.getString(Constants.Prefs.SQL_DATABASE, ""));
-        binding.editUser.setText(Globals.sp.getString(Constants.Prefs.SQL_USER, ""));
-        binding.editPassword.setText(Globals.sp.getString(Constants.Prefs.SQL_PASSWORD, ""));
+        binding.editGoogleDrive.setText(Globals.sp.getString(Constants.Prefs.GOOGLE_DRIVE, "1DB3Dg9N-4zbOxV28Sor-YJrWuoK_OfY8"));
+        binding.editServer.setText(Globals.sp.getString(Constants.Prefs.SQL_SERVER, "mssql01.cpr3663.io"));
+        binding.editDatabase.setText(Globals.sp.getString(Constants.Prefs.SQL_DATABASE, "CPR_Scouting_2025"));
+        binding.editUser.setText(Globals.sp.getString(Constants.Prefs.SQL_USER, "CPR_Tablet"));
+        binding.editPassword.setText(Globals.sp.getString(Constants.Prefs.SQL_PASSWORD, "Aa98o1nTlHb4sg2u0YB2eNHxVfU4n17z"));
     }
 
     // =============================================================================================
